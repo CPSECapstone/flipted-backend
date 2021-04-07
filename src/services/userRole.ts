5fd81e4cde8c25ccb844164de674840fedd9dedc
Destin Estrela <destin.estrela@unity3d.com>
2021-04-06 17:59:52 -0700

    Added helper function userTypeService.getUserRole(user_id : string)

---
+++ src/services/userRole.ts
@@ -0,0 +1,49 @@
+import { marshall, unmarshall, marshallOptions } from "@aws-sdk/util-dynamodb";
+import {
+  DynamoDBClient,
+  GetItemCommand,
+  GetItemCommandOutput,
+  PutItemCommand,
+  PutItemInput,
+  PutItemCommandInput,
+  PutItemCommandOutput
+} from "@aws-sdk/client-dynamodb";
+import { environment } from "../environment";
+
+const client = new DynamoDBClient({ region: "us-east-1" });
+const USER_TABLE = "Users-" + environment.stage;
+
+const marshallOpts: marshallOptions = {
+  removeUndefinedValues: true,
+  convertEmptyValues: false,
+  convertClassInstanceToMap: true,
+};
+
+/** 
+ * Returns the role of the user given the user id contained in the JWT access token
+ * Should be either "instructor" or "student"
+ */
+async function getUserRole(user_id: string) {
+  const command = new GetItemCommand({
+    TableName: USER_TABLE,
+    Key: marshall({
+      id: user_id
+    }, marshallOpts)
+  });
+  try {
+    const output: GetItemCommandOutput = await client.send(command);
+    if (output.Item) {
+      return unmarshall(output.Item).userRole;
+    }
+    throw new Error(`user_id=${user_id} not found`);
+  } catch (err) {
+    console.error(err);
+    return err;
+  }
+}
+
+const userTypeService = {
+  getUserRole,
+}
+
+export default userTypeService;
