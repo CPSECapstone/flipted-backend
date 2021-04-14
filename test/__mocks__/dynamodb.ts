6f9a8a512e5650f5a933f28fb657b3536b1a63f0
zzheng07 <55859981+zzheng07@users.noreply.github.com>
2021-04-13 20:27:25 -0700

    Feat dynamodb test (#15)
    * changed table name and added test cases
    
    * added dynamodb mock

---
+++ test/__mocks__/dynamodb.ts
@@ -0,0 +1,16 @@
+export const awsSkdPromiseResponse = jest.fn().mockReturnValue(Promise.resolve(true));
+
+const putFn = jest.fn().mockImplementation(() => {
+  promise: awsSkdPromiseResponse
+});
+
+const getFn = jest.fn().mockImplementation(() => {
+  promise: awsSkdPromiseResponse
+});
+
+const dynamodbMock = {
+  put: putFn,
+  get: getFn
+};
+
+export default dynamodbMock;
