f6ff9f56bc3d61a264af2de2131f704758abbe23
Destin Estrela <destin.estrela@gmail.com>
2021-04-15 16:58:38 -0700

    Storing and Applying Progress Towards Tasks + AddTask error checking (#16)
    * Wrote SubmitTaskProgress mutation and resolver (no verification)
    
    * Added task bus logic to verify that a submission is valid
    
    * Updated bus logic and tests for validating task submission
    
    * Fixed missing return in resolver
    
    * Added applyTaskProgress buisness logic function
    
    * Whe querying for a task, apply task progress if it exists
    
    * Added docs for submitting task progress
    
    * Remove mistakenly copied comment.
    
    * PR changes and cleanup
    
    * Throw if task input block is using incorrect type enum
    
    * Explicit typing instead of any for TypeBlocks
    
    Co-authored-by: Destin Estrela <destin.estrela@unity3d.com>

--- src/schema/task.resolver.ts
+++ src/schema/task.resolver.ts
@@ -1,23 +1,51 @@
-import { TaskInput } from '../interfaces/taskInterfaces';
+import { Task, TaskInput, TaskProgress} from '../interfaces/taskInterfaces';
 import { validateToken } from '../jws-verifer';
 import taskService from '../services/task';
 import userTypeService from '../services/userRole';
+import taskBusLogic from '../buslogic/taskBusLogic'
 
 async function addTask(_: any, args: any, context: any, info: any) {
-   const tokenPayload = await validateToken(context.headers.Authorization) // first validate the JWT token
+   const tokenPayload = await validateToken(context.headers.Authorization) 
    const userRole = await userTypeService.getUserRole(tokenPayload.username) // then get the user role
 
    if (userRole == "instructor") {
       const task: TaskInput = args.task;
-      return taskService.add(task, );
+      return taskService.add(task);
    } else {
      return Error("User is not an instructor")
    }
 }
 
+async function submitTaskProgress(_: any, args: any, context: any, info: any) {
+   const tokenPayload = await validateToken(context.headers.Authorization) 
+   
+   const taskProgress: TaskProgress = {
+      username: tokenPayload.username,
+      ...args.taskProgress
+   }
+
+   // verify that the list of completed requirement ids exist in the task
+   const task: Task = await taskService.getTaskById(taskProgress.taskId)
+   if(taskBusLogic.areTaskProgressIdsValid(task, taskProgress))
+   {
+      return taskService.updateTaskProgress(taskProgress)
+   }
+
+   return Error("Failed to verify ids contained in task submission")
+}
+
 async function getTaskById(_: any, args: any, context: any, info: any) {
-  const taskId = args.taskId;
-  return taskService.getById(taskId);
+   const tokenPayload = await validateToken(context.headers.Authorization) 
+   const task: Task = await taskService.getTaskById(args.taskId);
+
+   try {
+      // apply any existing task progress
+      const taskProgress = await taskService.getTaskProgress(task.id, tokenPayload.username)
+      return taskBusLogic.applyTaskProgress(task, taskProgress);
+    } catch (err) {
+      // no existing task progress, return task in its current form
+      return task;
+    }
 }
 
 async function listTasksBySubmissionId(_: any, args: any, context: any, info: any) {
@@ -49,7 +77,8 @@ const resolvers = {
     tasks: listTasksBySubmissionId
   },
   Mutation: {
-    addTask: addTask
+    addTask: addTask,
+    submitTaskProgress: submitTaskProgress
   }
 };
 
