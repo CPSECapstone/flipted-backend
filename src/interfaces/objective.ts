6815e91b9d268038e0fc2bd9e37b0bb77ae9e707
zzheng07 <zzheng07@calpoly.edu>
2021-05-02 22:44:01 -0700

    update objective interface methods

--- src/interfaces/objective.ts
+++ src/interfaces/objective.ts
@@ -1,15 +1,11 @@
 import { CompositeDBItem } from "../services/dynamodb";
 
-export function DBKey(prefix: string, id: string) {
-   return `${prefix}#${id}`;
-}
-
 export type ObjectiveItem = CompositeDBItem & Omit<Objective, "__typename" | "tasks">;
 
 export function ObjectivePK(course: string): string {
-   return DBKey("COURSE", course);
+   return `COURSE#${course}`;
 }
 
 export function ObjectiveSK(objectiveId: string): string {
-   return DBKey("OBJECTIVE", objectiveId);
+   return `OBJECTIVE#${objectiveId}`;
 }
