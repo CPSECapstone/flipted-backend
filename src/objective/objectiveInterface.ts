c890de01aee99cf89908cd2fd655e13bd2cf9244
Destin Estrela <destin.estrela@gmail.com>
2021-05-13 19:02:48 -0700

    Update objectiveInterface.ts

--- src/objective/objectiveInterface.ts
+++ src/objective/objectiveInterface.ts
@@ -4,14 +4,6 @@ export type ObjectiveItem = CompositeDBItem &
    Omit<Objective, "__typename" | "tasks"> & {
       taskIds: string[];
    };
-   objectiveId: string,
-   objectiveName: string;
-   description: string;
-   targetId: string;
-   targetName: string;
-   course: string;
-   tasks: string[]
-}
 
 export const ObjectivePrefix = "OBJECTIVE";
 
