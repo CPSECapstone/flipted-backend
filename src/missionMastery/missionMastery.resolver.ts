1e01fcd181847f8088f33a80cb919adc429b476d
zzheng07 <zzheng07@calpoly.edu>
2021-05-19 16:21:58 -0700

    fixed typo

--- src/missionMastery/missionMastery.resolver.ts
+++ src/missionMastery/missionMastery.resolver.ts
@@ -1,12 +1,12 @@
 import * as service from "./missionMsteryService";
 
-async function queryClasssMissionMastery(_: any, args: QueryClassMissionMasteryArgs) {
+async function queryClassMissionMastery(_: any, args: QueryClassMissionMasteryArgs) {
    return service.queryClassMissionMastery(args.courseId, args.missionId);
 }
 
 const resolvers = {
    Query: {
-      classMissionMastery: queryClasssMissionMastery
+      classMissionMastery: queryClassMissionMastery
    }
 };
 
